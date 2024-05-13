4c2d9b0f1a46a43681686a7ce6dd4bd5a720f9db
Ezmyrian <56850288+Ezmyrian@users.noreply.github.com>
2024-05-13 18:29:26 -0500

    Write createGrid function

---
+++ script.js
@@ -0,0 +1,18 @@
+function createGrid() {
+  const body = document.querySelector('body');
+  const grid = document.createElement('div');
+  grid.classList.add('main');
+  
+  for (i = 0; i < 16; i++) {
+    const row = document.createElement('div');
+    for (j = 0; j <16; j++) {
+      const column = document.createElement('div');
+      column.classList.add('square');
+      row.appendChild(column);
+    }
+    grid.appendChild(row);
+  }
+  body.appendChild(grid);
+}
+
+createGrid()
