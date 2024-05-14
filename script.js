bcc5d00801dd8d34cba5f3130f19e93855192261
Ezmyrian <56850288+Ezmyrian@users.noreply.github.com>
2024-05-14 18:31:34 -0500

    Change main class name to grid to better reflect design and add row class for css styling

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 function createGrid() {
   const body = document.querySelector('body');
   const grid = document.createElement('div');
-  grid.classList.add('main');
+  grid.classList.add('grid');
   
   for (i = 0; i < 16; i++) {
     const row = document.createElement('div');
+    row.classList.add('row');
     for (j = 0; j <16; j++) {
       const column = document.createElement('div');
       column.classList.add('square');
