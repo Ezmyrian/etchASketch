0ccb77fe956d0ba271ab9ef78499481d4af6606e
Ezmyrian <56850288+Ezmyrian@users.noreply.github.com>
2025-04-05 11:14:15 -0500

    Refactor code into functions
    Move container variable into createDiv function as it isn't needed
    in global scope.
    Add class child to columns and rows to facilitate div removal during
    grid reset.
    Create function for creating mouseenter event listener so it can be
    called repeatedly instead of only during initial page load.
    Create function to add header content and allow for dynamic grid size
    display for when user resets grid.
    Create function to create new grid button and add click event listener
    to remove current grid and create new one by calling the relevant
    functions.
    Create function to remove current content before new content is created.
        Without removing old content, new content would be added instead of
        replacing the current content.

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
-let container = document.querySelector(".container");
-
 function createDivs(size = 16) {
+    let container = document.querySelector(".container");
     for (let i = 1; i <= size; i++) {
         let columnDiv = document.createElement('div');
-        columnDiv.classList.add(`column${i}`);
+        columnDiv.classList.add(`column${i}`, `child`);
         columnDiv.style.cssText = "display: flex; flex-direction: column; flex: 1 1 auto;"
         container.appendChild(columnDiv);
         for (let j = 1; j <= size; j++) {
             let column = document.querySelector(`.column${i}`);
             let rowDiv = document.createElement('div');
-            rowDiv.classList.add("row");
+            rowDiv.classList.add("row", "child");
             rowDiv.style.cssText = "display: flex; flex: 1 1 auto; border: black 1px solid;"
             column.appendChild(rowDiv);
         }
@@ -17,9 +16,54 @@ function createDivs(size = 16) {
 }
 createDivs()
 
-let divs = document.querySelectorAll('.row');
-divs.forEach((div) => {
-    div.addEventListener('mouseenter', () => {
-        div.style.backgroundColor = "black"
+function addBackgroundColorChanger() {
+    let divs = document.querySelectorAll('.row');
+    divs.forEach((div) => {
+        div.addEventListener('mouseenter', () => {
+            div.style.backgroundColor = "black"
+        })
+    });
+}
+addBackgroundColorChanger()
+
+function addHeaderContent (newGridSize = 16) {
+    const header = document.querySelector('.header');
+    const text = document.createElement('p');
+    text.classList.add("text");
+    text.textContent = `Grid size: ${newGridSize}`
+    header.append(text);
+    addNewGridButton();
+}
+addHeaderContent();
+
+function addNewGridButton () {
+    const header = document.querySelector(".header");
+    const newGridButton = document.createElement('button');
+    newGridButton.textContent = "New Grid";
+    newGridButton.style.backgroundColor = "white";
+    header.append(newGridButton);
+    newGridButton.addEventListener('click', () => {
+        let newGridSize = +prompt("How big would you like the grid to be?", "Default 16, Max 100");
+        if (newGridSize < 16 || newGridSize == NaN) {
+            newGridSize = 16;
+        }
+        else if (newGridSize > 100) {
+            newGridSize = 100;
+        }
+        
+        removeContent()
+        createDivs(newGridSize);
+        addBackgroundColorChanger()
+        addHeaderContent(newGridSize)
     })
-});
+}
+
+function removeContent() {
+    let childDivs = document.querySelectorAll(".child");
+    childDivs.forEach((child) => {
+        child.remove();
+    })
+
+    let header = document.querySelector(".header");
+    header.replaceChildren();
+}
